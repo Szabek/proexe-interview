fbf79d27298cb5984b188955d53d37f2a225bcf5
DChabowski <mrschabpl@gmail.com>
2024-05-28 14:31:06 +0200

    getTitles in MovieController

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,15 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Movies\MovieAggregatorService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
+
+    protected MovieAggregatorService $movieAggregatorService;
+
+    public function __construct(MovieAggregatorService $movieAggregatorService)
+    {
+        $this->movieAggregatorService = $movieAggregatorService;
+    }
     public function getTitles(Request $request): JsonResponse
     {
-        // TODO
-
-        return response()->json([]);
+        try {
+            $titles = $this->movieAggregatorService->getAggregatedTitles();
+            return response()->json($titles);
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'failure']);
+        }
     }
 }
